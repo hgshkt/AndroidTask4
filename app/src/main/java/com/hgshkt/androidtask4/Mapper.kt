1f7cb559a073d18ddc900b120b863d1ff4d15443
hgshkt <nichtojestvozxc@gmail.com>
2024-01-24 23:11:43 +0200

    weather to display weather mapper

---
+++ app/src/main/java/com/hgshkt/androidtask4/Mapper.kt
@@ -0,0 +1,10 @@
+package com.hgshkt.androidtask4
+
+import com.hgshkt.androidtask4.api.model.Weather
+import com.hgshkt.androidtask4.model.DisplayWeather
+
+fun Weather.toDisplay(): DisplayWeather {
+    return DisplayWeather(
+        temperature = temperature
+    )
+}
